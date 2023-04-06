3a52e3988d320daa4bd9c6df9268a9bb1d53845b
Rife Develop <61239846+rifedevelop@users.noreply.github.com>
2023-04-06 15:16:30 +0700

    Revert "Dasep"

--- Modules/GuardTour/Http/Controllers/GuardTourController.php
+++ Modules/GuardTour/Http/Controllers/GuardTourController.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
-class PlantsController extends Controller
+class GuardTourController extends Controller
 {
     /**
      * Display a listing of the resource.
